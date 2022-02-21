5cfaa14895af08a83bf8bc1561fabdcfef02f946
zwb <673207325@qq.com>
2022-02-21 08:56:52 +0800

    master commit2

--- demo1/src/main/java/com/git/demo1/HelloServlet.java
+++ demo1/src/main/java/com/git/demo1/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!,Hello,Java!,Hello,Git1,hello,zwb!";
+        message = "Hello World!,Hello,Java!,Hello,Git1,hello,zwb!,hello,master!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
